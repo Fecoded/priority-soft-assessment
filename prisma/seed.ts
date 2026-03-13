d88a087723618539112aaa152c9d475f2a4c4d28
fecoded <tojuofe@gmail.com>
2026-03-13 10:43:15 +0100

    fixes changes to postgres

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,11 @@
 import bcrypt from "bcryptjs";
 import { PrismaClient, Role } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+// import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+// const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
 
 const prisma = new PrismaClient({
-  adapter,
+  // adapter,
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "info", "warn", "error"]
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,14 @@
 // import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
 // const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
 
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
 const prismaClientSingleton = () =>
   new PrismaClient({
-    // adapter,
+    adapter,
     log:
       process.env.NODE_ENV === "development"
         ? ["query", "info", "warn", "error"]
