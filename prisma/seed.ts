687a8b38b149e1492283a728cc7e60a3cd2b9a92
fecoded <tojuofe@gmail.com>
2026-03-13 10:52:01 +0100

    commit change on seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,12 @@
 import bcrypt from "bcryptjs";
 import { PrismaClient, Role } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 // import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-// const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const prisma = new PrismaClient({
-  // adapter,
+  adapter,
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "info", "warn", "error"]
